9874646c63f9de875dd975fb624eb6844d2e041d
eugeneLuck <eugeneLuck@gmail.com>
2023-02-25 14:03:06 +0300

    Add project files.

---
+++ CreateLibrary01.sql
@@ -0,0 +1,16 @@
+﻿use Library01
+create table Authors
+(
+	Id int not null identity(1,1) primary key,
+	FirstName varchar(20) not null,
+	LastName varchar(20) not null
+)
+create table Books
+(
+	Id int not null identity(1,1) primary key,
+	AuthorId int not null,
+	foreign key (AuthorId) references Authors (id),
+	Title varchar (100) not null,
+	Price int,
+	Pages int
+)
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+using static System.Console;
+
+SqlConnection connection = null;
+try
+{
+    string connectionString = 
+        @"Data Source=(localdb)\MSSQLLocalDB;" +
+        "Initial Catalog=Library01;" +
+        "Integrated Security=True";
+    connection = new SqlConnection(connectionString);
+    connection.Open();
+
+    string commandText = 
+        "insert Authors values " +
+        "('Paul', 'Deitel')," +
+        "('John', 'Deitel')";
+    SqlCommand command = 
+        new SqlCommand(commandText, connection);
+    command.ExecuteNonQuery();
+    WriteLine("Success.");
+}
+catch(Exception ex)
+{ 
+    Console.WriteLine(ex.Message); 
+}
+finally
+{
+    if (connection is not null)
+    {
+        connection.Close();
+    }
+    ReadKey();
+}
+
